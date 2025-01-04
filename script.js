a10b36754c1aa36bbd416acb3ded32d3489c2528
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-04 15:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function insert_Row() {
-    //Write your code here
-	function insert_Row() {
     var table = document.getElementById("sampleTable");
-    var row = table.insertRow(0);
-}
-  
-  
+    var row = table.insertRow(0); 
+    
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "New Cell1"; 
+    cell2.innerHTML = "New Cell2";
 }
